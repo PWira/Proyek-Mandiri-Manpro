ee3f2693668f044ae3176f99b7d49eb41d536d22
pwira1 <personalputra06@gmail.com>
2023-12-29 16:16:25 +0700

    add home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Novel & Komik</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Font Awesome for free icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+  <!-- Google Fonts - Montserrat and Poppins -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Poppins:wght@400;700&display=swap">
+  <style>
+    body {
+      font-family: 'Montserrat', sans-serif;
+    }
+    h1, h2, h3, h4, h5, h6 {
+      font-family: 'Poppins', sans-serif;
+    }
+    /* Custom styles for the header, navigation, main content, and footer */
+    header {
+      background-color: #343a40;
+      color: #ffffff;
+      padding: 15px 0;
+      text-align: center;
+    }
+    nav {
+      background-color: #f8f9fa;
+      padding: 10px 0;
+    }
+    main {
+      padding: 20px 0;
+    }
+    footer {
+      background-color: #343a40;
+      color: #ffffff;
+      padding: 15px 0;
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+
+  <!-- Header -->
+  <header>
+    <h1>Novel & Komik</h1>
+  </header>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">Beranda</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Novel</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Komik</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Kategori Lainnya</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Main Content -->
+  <main class="container">
+    <!-- Your main content goes here -->
+    <h2>Selamat datang di dunia cerita yang menarik!</h2>
+    <p>Temukan kisah-kisah seru dalam novel dan komik terbaru.</p>
+  </main>
+
+  <!-- Footer -->
+  <footer>
+    <p>&copy; 2023 Novel & Komik. All rights reserved.</p>
+    <p>About Us</p>
+  </footer>
+
+  <!-- Bootstrap JS and Popper.js (optional) -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
