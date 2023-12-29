a0870731aaf812e8dad83ba35d568346efc7df56
pwira1 <personalputra06@gmail.com>
2023-12-29 15:58:19 +0700

    add home blade

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/_laravel_template', function () {
     return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('home');
 });
